b6ee5bfdac793302c586e73973994dfd5d5126a7
Phil <plandry@uoguelph.ca>
2019-03-20 17:37:14 -0500

    Fixing sqm EoS code

--- sqm/lib/get_sqm.py
+++ sqm/lib/get_sqm.py
@@ -9,21 +9,25 @@ import numpy as np
 import scipy.integrate as integrate
 import scipy.interpolate as interp
 
-def sqm(B, cqm2, stp=1e2, pmax=1e17, eps=1e-4): # bag constant B in eV, SQM sound speed squared in c^2
+def sqm(B, cqm2, stp=1e2, pmax=1e17, eps=1e-4): # bag constant B in MeV, SQM sound speed squared in c^2
+
+	MeVc2tog = 1.78266191e-27
+	fmtocm = 1e-13
+	fac = MeVc2tog/fmtocm**3
 
 # DEFINE CORE PARAMETERS
 
 	def mu(p): # total energy density in g/cm^3 as a function of pressure in g/cm^3
 	
-		return 4.*B**4 + p/cqm2
+		return 4.*B*fac + p/cqm2
 		
 	def rho(p): # rest-mass energy density in g/cm^3, matching rho and mu at surface p=0
 	
 		num = 4.**cqm2*(mu(p)+p)*cqm2
-		denom = cqm2*B**4
+		denom = cqm2*B*fac
 		power = 1./(1.+cqm2)
 	
-		return B**4*(num/denom)**power
+		return B*fac*(num/denom)**power
 
 # EXPORT EOS DATA
 
